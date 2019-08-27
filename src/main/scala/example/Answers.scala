35c3bfd6e3243ed04c968c110e84d814d3b0e3bc
kazuki <pharcydetip@gmail.com>
2019-08-28 00:49:13 +0900

    tailrec

--- src/main/scala/example/Answers.scala
+++ src/main/scala/example/Answers.scala
@@ -1,9 +1,13 @@
 package example
 
 object Answers {
-  def fib(n: Int): Int = n match {
-    case num if num > 1 => fib(num-1) + fib(num-2)
-    case num if num == 1 => 1
-    case num if num == 0 => 0
+  def fib(n: Int): Int = {
+    def calcFib(n: Int, value1: Int = 1, value2: Int): Int = n match {
+      case _ if n > 1 => calcFib(n-1, value1+value2, value1)
+      case _ if n == 1 => value1
+      case _ if n == 0 => 0
+    }
+
+    calcFib(n, 1, 0)
   }
 }
