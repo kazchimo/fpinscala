2ec08685c051550413c7ab2e77cb4ebb1d06ebf7
kazuki <pharcydetip@gmail.com>
2019-08-30 17:30:55 +0900

    implement curry

--- src/main/scala/example/Answers.scala
+++ src/main/scala/example/Answers.scala
@@ -3,11 +3,18 @@ package example
 object Answers {
   def fib(n: Int): Int = {
     def calcFib(n: Int, value1: Int = 1, value2: Int): Int = n match {
-      case _ if n > 1 => calcFib(n-1, value1+value2, value1)
+      case _ if n > 1 => calcFib(n - 1, value1 + value2, value1)
       case _ if n == 1 => value1
       case _ if n == 0 => 0
     }
 
     calcFib(n, 1, 0)
   }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean =
+    as.slice(0, as.length-1).zipWithIndex.foldLeft[Boolean](true) {
+      case (a, (e, index)) => a && ordered(e, as(index + 1))
+    }
+
+  def curry[A, B, C](f: (A, B) => C): A => B => C = (a: A) => (b: B) => f(a, b)
 }
