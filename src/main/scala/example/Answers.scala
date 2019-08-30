e878ca18e14e161179f2ea63cbceb119b638efed
kazuki <pharcydetip@gmail.com>
2019-08-30 17:37:47 +0900

    implement uncurry

--- src/main/scala/example/Answers.scala
+++ src/main/scala/example/Answers.scala
@@ -17,4 +17,6 @@ object Answers {
     }
 
   def curry[A, B, C](f: (A, B) => C): A => B => C = (a: A) => (b: B) => f(a, b)
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a: A, b: B) => f(a)(b)
 }
